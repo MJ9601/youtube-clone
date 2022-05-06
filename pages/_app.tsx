405681f88bbc63ae94c75d86ad7595c8cfc759ff
mj khodadadi <m_javad_96@yahoo.com>
2022-05-06 11:35:27 -0700

    5/6/2022 1135AM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
+import { NotificationsProvider } from "@mantine/notifications";
+import Head from "next/head";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  <>
+    <Head>
+      <title>YouTube clone</title>
+      <meta
+        name="viewport"
+        content="minimum-scale=1, initial-scale=1, width=device-width"
+      />
+    </Head>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <NotificationsProvider>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </NotificationsProvider>
+    </MantineProvider>
+    ;
+  </>;
 }
 
-export default MyApp
+export default MyApp;
